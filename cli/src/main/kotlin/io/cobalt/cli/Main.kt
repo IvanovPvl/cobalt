50ea454c9af735eb8b7f83c682c8de16349e2ebb
Pavel Ivanov <tyrension@gmail.com>
2019-01-27 20:02:10 +0700

    Add `print-chain` and `add block` commands

--- cli/src/main/kotlin/io/cobalt/cli/Main.kt
+++ cli/src/main/kotlin/io/cobalt/cli/Main.kt
@@ -1,16 +1,37 @@
 package io.cobalt.cli
 
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+
 import io.cobalt.core.Blockchain
 import io.cobalt.core.storage.RocksDbBucket
 import io.cobalt.core.storage.DefaultSerializer
 
-fun main(args: Array<String>) {
-    val store = RocksDbBucket(DefaultSerializer())
-    val blockchain = Blockchain(store)
-    blockchain.addBlock("Send 1 BTC to Pavel")
-    blockchain.addBlock("Send 3 BTC to Ivan")
+class BlockchainCommand : CliktCommand() {
+    override fun run() = Unit
+}
 
-    for (block in blockchain) {
-        print(block)
+class PrintChainCommand : CliktCommand(name = "print-chain", help = "Print the blockchain") {
+    override fun run() {
+        val store = RocksDbBucket(DefaultSerializer())
+        val blockchain = Blockchain(store)
+        for (block in blockchain) {
+            print(block)
+        }
     }
 }
+
+class AddBlockCommand : CliktCommand(name = "add-block", help = "Add block to blockchain") {
+    private val data: String? by option("-d", "--data")
+
+    override fun run() {
+        val store = RocksDbBucket(DefaultSerializer())
+        val blockchain = Blockchain(store)
+        this.data?.let { d -> blockchain.addBlock(d)}
+    }
+}
+
+fun main(args: Array<String>) = BlockchainCommand()
+    .subcommands(PrintChainCommand(), AddBlockCommand())
+    .main(args)
